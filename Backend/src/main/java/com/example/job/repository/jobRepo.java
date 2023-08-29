055ac8b09c518f06cb840434739bc8f42bbd83ec
Sithum Dilshan <shanukasithum28@gmail.com>
2023-08-29 18:49:05 +0530

    create job repo

---
+++ Backend/src/main/java/com/example/job/repository/jobRepo.java
@@ -0,0 +1,16 @@
+package com.example.job.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.job.model.job;
+
+public interface jobRepo extends JpaRepository<job, Long>{
+
+
+	@Query(value = "SELECT DISTINCT category FROM job", nativeQuery = true)
+	List<String> findDistinctCategories();
+	
+}
