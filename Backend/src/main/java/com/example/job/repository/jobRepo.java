4b19cb790657b16846041e214550851695229f57
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-03 18:48:02 +0530

    update availability repo

--- Backend/src/main/java/com/example/job/repository/jobRepo.java
+++ Backend/src/main/java/com/example/job/repository/jobRepo.java
@@ -4,16 +4,24 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.job.model.job;
 
 public interface jobRepo extends JpaRepository<job, Long>{
 
 
-	@Query(value = "SELECT DISTINCT category FROM job", nativeQuery = true)
-	List<String> findDistinctCategories();
-	
-	@Query(value = "SELECT category, COUNT(*) as duplicateCount FROM job GROUP BY category", nativeQuery = true)
-    List<Object[]> findDistinctCategoriesWithDuplicateCount();
+	@Query(value = "SELECT category, COUNT(*) as duplicateCount " +
+            "FROM job " +
+            "GROUP BY category " +
+            "HAVING COUNT(*) >= 1", nativeQuery = true)
+List<Object[]> findDistinctCategoriesWithDuplicateCount();
+
+	@Query(value = "SELECT * FROM job ORDER BY RAND() LIMIT 1", nativeQuery = true)
+	String getRandomJobTitle();
+
+	 @Query(value = "SELECT * FROM job WHERE country = :country AND category = :category", nativeQuery = true)
+	    List<job> findByCountryAndCategory(@Param("country") String country, @Param("category") String category);
+
 	
 }
