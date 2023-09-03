2679e2482b4d7e98b18be4665691c5bb7c32a5af
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-03 18:47:37 +0530

    update availability repo

--- Backend/src/main/java/com/example/job/repository/availabilityRepo.java
+++ Backend/src/main/java/com/example/job/repository/availabilityRepo.java
@@ -1,9 +1,21 @@
 package com.example.job.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.job.model.availability;
 
 public interface availabilityRepo extends JpaRepository<availability, Long> {
-
+	
+	@Query(value = "SELECT * FROM availability " +
+            "WHERE date = :selectedDate " +
+            "AND timeslot = :selectedTimeslot", nativeQuery = true)
+List<availability> findByDateAndTimeslot(
+     @Param("selectedDate") String selectedDate,
+     @Param("selectedTimeslot") String selectedTimeslot);
 }
+
+
