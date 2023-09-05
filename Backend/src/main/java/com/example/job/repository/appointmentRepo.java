82ba6c2664d75a1c1fe8ccf2870d7f084df68438
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-06 02:38:55 +0530

    update backend

--- Backend/src/main/java/com/example/job/repository/appointmentRepo.java
+++ Backend/src/main/java/com/example/job/repository/appointmentRepo.java
@@ -1,9 +1,19 @@
 package com.example.job.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.job.model.appointment;
 
 public interface appointmentRepo extends JpaRepository<appointment, Long>{
+	
+	 @Query(value = "SELECT COUNT(*) FROM appointment", nativeQuery = true)
+	    long countAllAppointments();
 
+	 
+	 @Query(value = "SELECT * FROM appointment WHERE email = :email", nativeQuery = true)
+	    List<appointment> findByCounsultEmail(@Param("email") String email);
 }
