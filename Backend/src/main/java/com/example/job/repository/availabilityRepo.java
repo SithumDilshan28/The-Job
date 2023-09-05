c46ea4097e6010cefc8807de3bc7f07705f9ce81
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-06 02:38:59 +0530

    update backend

--- Backend/src/main/java/com/example/job/repository/availabilityRepo.java
+++ Backend/src/main/java/com/example/job/repository/availabilityRepo.java
@@ -16,6 +16,9 @@ public interface availabilityRepo extends JpaRepository<availability, Long> {
 List<availability> findByDateAndTimeslot(
      @Param("selectedDate") String selectedDate,
      @Param("selectedTimeslot") String selectedTimeslot);
+
+
+
 }
 
 
