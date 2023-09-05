81019f2f7154ae666d32a1f16b36bbc90e4acd45
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-06 02:42:08 +0530

    connect backend and frontend

---
+++ Frontend/Consultant Dashboard/api/appointment.js
@@ -0,0 +1,34 @@
+getAllappointment();
+
+function getAllappointment() {
+  const email = sessionStorage.getItem("email");
+  $.ajax({
+      method: "POST",
+      url: "http://localhost:8080/appointment/consultant-email/"+email,
+      async: true,
+      success: function (data) {
+          populateTable(data);
+      },
+      error: function (xhr, exception) {
+          alert("Error");
+      },
+  });
+}
+
+
+function populateTable(data) {
+  var tableBody = $("#prodTable");
+  // Clear the existing table body
+  tableBody.empty();
+  // Loop through the data and append rows to the table
+  $.each(data, function (index, element) {
+      var row = $("<tr>");
+      row.append($("<td>").text(element.id));
+      row.append($("<td>").text(element.consultantname));
+      row.append($("<td>").text(element.clientname));
+      row.append($("<td>").text(element.clientphone));
+      row.append($("<td>").text(element.date));
+      row.append($("<td>").text(element.time));
+      tableBody.append(row);
+  });
+}
