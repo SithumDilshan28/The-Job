2bef47fbefe44ebb115029be5c757e12f2f5acbf
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-06 02:39:13 +0530

    update backend

--- Backend/src/main/java/com/example/job/repository/userRepo.java
+++ Backend/src/main/java/com/example/job/repository/userRepo.java
@@ -8,4 +8,11 @@ import com.example.job.model.user;
 public interface userRepo extends JpaRepository<user, Long> {
 	 @Query(value = "SELECT * FROM user WHERE email = :email AND password = :password", nativeQuery = true)
 	    user findByEmailAndPassword(@Param("email") String name, @Param("password") String password);
+	 
+	 @Query(value = "SELECT COUNT(*) FROM user", nativeQuery = true)
+	    long countAllUsers();
+	 
+	 @Query(value = "SELECT * FROM user WHERE email = :email", nativeQuery = true)
+	 user findByEmail(@Param("email") String email);
+
 }
