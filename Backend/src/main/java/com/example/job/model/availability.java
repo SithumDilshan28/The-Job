fc6bbc788668adfc851e593507d7d0aab06c8563
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-06 02:38:51 +0530

    update backend

--- Backend/src/main/java/com/example/job/model/availability.java
+++ Backend/src/main/java/com/example/job/model/availability.java
@@ -20,40 +20,154 @@ public class availability {
 	
 	@Column(name = "timeslot")
 	private String timeslot;
+	
+	@Column(name = "fullname")
+	private String fullname;
+	
+	@Column(name = "imageurl", length = 5000) // Set the length to 5000 characters
+    private String imageurl;
 
-	public availability(long id, String date, String timeslot) {
+	@Column(name = "specilizedjobtype")
+	private String specilizedjobtype;
+	
+	@Column(name = "specilizedcountry")
+	private String specilizedcountry;
+	
+	@Column(name = "phone")
+	private String phone;
+	
+	@Column(name = "email")
+	private String email;
+	
+	
+
+	public availability(long id, String date, String timeslot, String fullname, String imageurl,
+			String specilizedjobtype, String specilizedcountry, String phone, String email) {
 		super();
 		Id = id;
 		this.date = date;
 		this.timeslot = timeslot;
+		this.fullname = fullname;
+		this.imageurl = imageurl;
+		this.specilizedjobtype = specilizedjobtype;
+		this.specilizedcountry = specilizedcountry;
+		this.phone = phone;
+		this.email = email;
 	}
-	
+
+
+
 	public availability () {
 		
 	}
+
 
 	public long getId() {
 		return Id;
 	}
 
+
+
 	public void setId(long id) {
 		Id = id;
 	}
+
+
 
 	public String getDate() {
 		return date;
 	}
 
+
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+	
+	
+	public String getFullname() {
+		return fullname;
+	}
+
+
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+
 
 	public String getTimeslot() {
 		return timeslot;
 	}
 
+
+
 	public void setTimeslot(String timeslot) {
 		this.timeslot = timeslot;
 	}
+
+
+
+	public String getImageurl() {
+		return imageurl;
+	}
+
+
+
+	public void setImageurl(String imageurl) {
+		this.imageurl = imageurl;
+	}
+
+
+
+	public String getSpecilizedjobtype() {
+		return specilizedjobtype;
+	}
+
+
+
+	public void setSpecilizedjobtype(String specilizedjobtype) {
+		this.specilizedjobtype = specilizedjobtype;
+	}
+
+
+
+	public String getSpecilizedcountry() {
+		return specilizedcountry;
+	}
+
+
+
+	public void setSpecilizedcountry(String specilizedcountry) {
+		this.specilizedcountry = specilizedcountry;
+	}
+
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
 	
 }
