28a74ba374fd6b7c75946dbcacbec554ef23ba61
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-06 02:39:03 +0530

    update backend

--- Backend/src/main/java/com/example/job/repository/consultantRepo.java
+++ Backend/src/main/java/com/example/job/repository/consultantRepo.java
@@ -1,5 +1,7 @@
 package com.example.job.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +11,14 @@ public interface consultantRepo extends JpaRepository<consultant, Long> {
 	
 	@Query(value = "SELECT * FROM consultant WHERE email = :email AND password = :password", nativeQuery = true)
     consultant findByEmailAndPassword(@Param("email") String name, @Param("password") String password);
+	    
+	 @Query(value = "SELECT fullname, imageurl, specilizedjobtype, specilizedcountry, phone, email " +
+             "FROM consultant " +
+             "WHERE email = :email", nativeQuery = true)
+List<Object[]> findConsultantDataByEmail(@Param("email") String email);
+
+@Query(value = "SELECT COUNT(*) FROM consultant", nativeQuery = true)
+long countAllConsultants();
+
+
 }
