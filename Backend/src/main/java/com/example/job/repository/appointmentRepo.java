dd77954fa0a51e93cedaafb772958a3ccf17e12a
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-09 20:51:54 +0530

    connect backend and frontend

--- Backend/src/main/java/com/example/job/repository/appointmentRepo.java
+++ Backend/src/main/java/com/example/job/repository/appointmentRepo.java
@@ -13,7 +13,26 @@ public interface appointmentRepo extends JpaRepository<appointment, Long>{
 	 @Query(value = "SELECT COUNT(*) FROM appointment", nativeQuery = true)
 	    long countAllAppointments();
 
-	 
 	 @Query(value = "SELECT * FROM appointment WHERE email = :email", nativeQuery = true)
 	    List<appointment> findByCounsultEmail(@Param("email") String email);
+	    
+	    
+	    
+	    
+	    @Query(
+	            value = "SELECT a.consultantname, COUNT(*) AS appointment_count, c.specilizedcountry " +
+	                    "FROM appointment a " +
+	                    "LEFT JOIN consultant c ON a.consultantname = c.fullname " +
+	                    "GROUP BY a.consultantname, c.specilizedcountry " +
+	                    "ORDER BY appointment_count DESC",
+	            nativeQuery = true
+	        )
+	        List<Object[]> consultantperformance();
+	        
+	        
+	        @Query(
+	        	    value = "SELECT * FROM appointment WHERE date >= CURRENT_DATE() ORDER BY date ASC",
+	        	    nativeQuery = true
+	        	)
+	        	List<appointment> findUpcomingAppointmentsOrderByDateAsc();
 }
