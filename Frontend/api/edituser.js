5b7f7a10cc0cfb3b51dacfa92573a17881934638
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-09 21:02:23 +0530

    update edit user

--- Frontend/api/edituser.js
+++ Frontend/api/edituser.js
@@ -1,39 +1,46 @@
 function updateUser() {
   // Collect data from the form fields
-  const updatedUserData = {
-      firstname: $("#input1").val(),
-      lastname: $("#input2").val(),
-      company: $("#input3").val(),
-      phone: $("#input4").val(),
-      email: $("#input5").val(),
-      address: $("#input6").val()
-  };
+  const firstname = $("#input1").val();
+  const lastname = $("#input2").val();
+  const company = $("#input3").val();
+  const phone = $("#input4").val();
+  const email = $("#input5").val();
+  const address = $("#input6").val();
+
+  // Check if any of the fields are empty
+  if (!firstname || !lastname || !company || !phone || !email || !address) {
+    alert("Please fill in all fields.");
+    return; // Prevent the AJAX request if any field is empty
+  }
 
   // Get the userEmail from the session variable
   const userEmail = sessionStorage.getItem("userEmail");
 
   // Make an AJAX request to update the user data
   $.ajax({
-      type: "PUT",  // Use the appropriate HTTP method (PUT in this case)
-      url: `http://localhost:8080/user/updateuserbyemail/${userEmail}`,  // Replace with your API endpoint
-      contentType: "application/json",
-      data: JSON.stringify(updatedUserData),
-      success: function(data) {
-          // Handle success
-          console.log("User data updated:", data);
-          // Show a success pop-up alert
-          alert("User data updated successfully!");
-      },
-      error: function(error) {
-          // Handle error
-          console.error("Error updating user data:", error);
-          // Show an error pop-up alert
-          alert("Failed to update user data.");
-      }
+    type: "PUT",
+    url: `http://localhost:8080/user/updateuserbyemail/${userEmail}`,
+    contentType: "application/json",
+    data: JSON.stringify({
+      firstname,
+      lastname,
+      company,
+      phone,
+      email,
+      address,
+    }),
+    success: function (data) {
+      console.log("User data updated:", data);
+      alert("User data updated successfully!");
+    },
+    error: function (error) {
+      console.error("Error updating user data:", error);
+      alert("Failed to update user data.");
+    },
   });
 }
 
 // Attach the updateUser function to the button's click event
-$("#updateButton").on("click", function() {
+$("#updateButton").on("click", function () {
   updateUser();
 });
