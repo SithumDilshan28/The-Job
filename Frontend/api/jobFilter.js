6d0cb619628d4d22f24f507fe9c333bc6af73679
Sithum Dilshan <shanukasithum28@gmail.com>
2023-09-09 21:02:35 +0530

    update job filter

--- Frontend/api/jobFilter.js
+++ Frontend/api/jobFilter.js
@@ -2,6 +2,52 @@ document.addEventListener("DOMContentLoaded", function () {
     const submitButton = document.getElementById("submit");
     const resultDiv = document.getElementById("result");
 
+    // Function to fetch and display all jobs
+    function displayAllJobs() {
+        fetch("http://localhost:8080/job/getjob")
+            .then((response) => response.json())
+            .then((data) => {
+                resultDiv.innerHTML = ""; // Clear previous content
+
+                const rowDiv = document.createElement("div");
+                rowDiv.className = "row"; // Create a Bootstrap row
+
+                data.forEach((item) => {
+                    const colDiv = document.createElement("div");
+                    colDiv.className = "col-lg-4 col-md-6 col-sm-12 mb-4"; // Use Bootstrap col-* classes
+
+                    colDiv.innerHTML = `
+                        <div class="employers-list position-relative pb-3 pt-3 pl-2 pr-2 border rounded min-width-element">
+                            <img src="${item.imageurl}" alt="" class="mx-auto d-block shadow border rounded-circle" width="100" height="100">
+                            <div class="grid-list-desc text-center mt-3">
+                                <h5 class="mb-2"><a href="#" class="text-dark name">${item.role}</a></h5>
+                                <p class="text-muted mb-0"><i class="mdi mdi-domain mr-2"></i>${item.company}</p>
+                                <p class="text-muted mb-0"><i class="mdi mdi-calendar-clock mr-2"></i>${item.type}</p>
+                                <p class="text-muted mb-0"><i class="mdi mdi-currency-usd mr-2"></i>${item.salary}</p>
+                                <p class="text-muted mb-0"><i class="mdi mdi-briefcase-outline mr-2"></i>${item.experience}</p>
+                            </div>
+                            <div class="apply-button text-center mt-3">
+                                <a href="consultant.html "class="btn btn-primary btn-sm">More Info</a>
+                            </div>
+                            <div class="fav-collection fav-icon">
+                                <i class="mdi mdi-heart" title="Collect Now"></i>
+                            </div>
+                        </div>
+                    `;
+
+                    rowDiv.appendChild(colDiv);
+                });
+
+                resultDiv.appendChild(rowDiv); // Add the row to the resultDiv
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+
+    // Call the function to display all jobs when the page loads
+    displayAllJobs();
+
     submitButton.addEventListener("click", function (event) {
         event.preventDefault(); // Prevent the default form submission behavior
 
